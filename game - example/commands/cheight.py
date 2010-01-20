4da6d3a9c6b8cf7a0ea51461bc3938e68a26021c
Richard Tew <richard.m.tew@gmail.com>
2010-01-20 21:26:49 +0000

    - Moving the 'cheight' command to the game logic directory it actually works with.
    git-svn-id: http://sorrows-mudlib.googlecode.com/svn/trunk@78 a9b543af-0a4c-0410-9886-61d43edf9ebb

---
+++ game - example/commands/cheight.py
@@ -0,0 +1,22 @@
+
+import math
+from mudlib import Command
+
+class CHeight(Command):
+    __verbs__ = [ 'cheight' ]
+
+    def Run(self, arg):
+        cline = arg[0]
+        if not len(cline):
+            return self.shell.user.Tell('cheight <x> <y> <z>')
+        try:
+            coords = [ float(x.strip()) for x in cline.split(' ') ]
+        except ValueError:
+            return self.shell.user.Tell('Expected float arguments, got something else: '+ str(arg))
+
+        w = sorrows.world.example
+        height = w.GetSurfaceHeight(coords)
+        if height >= w.seaLevel:
+            self.shell.user.Tell('Result: %f (land)' % height)
+        else:
+            self.shell.user.Tell('Result: %f (water)' % height)
