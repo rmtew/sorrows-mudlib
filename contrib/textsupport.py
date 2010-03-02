41b9af3b1bd6878cc405ee43ce3d7236b47f8604
Richard Tew <richard.m.tew@gmail.com>
2010-03-02 05:29:26 +0000

    - contrib/textsupport.py: Added 'number_to_text' function which converts numbers like 192032 to 'nine hundred and two thousand and thirty two'.
    git-svn-id: http://sorrows-mudlib.googlecode.com/svn/trunk@139 a9b543af-0a4c-0410-9886-61d43edf9ebb

--- contrib/textsupport.py
+++ contrib/textsupport.py
@@ -2,7 +2,10 @@
 This module is supposed to do useful things in the area of text formatting.
 """
 
-import StringIO
+import StringIO, logging
+
+logger = logging.getLogger("textsupport")
+
 
 def hcolumns(strings, width=80, marginSize=1, leftMarginSize=2, columnSize=None):
     if columnSize is None:
@@ -21,3 +24,108 @@ def hcolumns(strings, width=80, marginSize=1, leftMarginSize=2, columnSize=None)
         sio.write((columnSize - len(s)) * " ")
         currentWidth += columnSize
     return sio.getvalue()
+
+
+def number_to_words(number):
+    if number == 0:
+        return "none"
+
+    if number < 20:
+        return [
+            "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen"
+        ][number]
+
+    if number < 100:
+        factor = (number / 10) - 2
+        remainder = number % 10
+        if remainder:
+            remainderString = " "+ number_to_string(remainder)
+        return [
+            "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        ][factor] + remainderString
+
+    if number < 1000:
+        factor = (number / 100)
+        remainder = number % 100
+        parts = [ number_to_words(factor) +" hundred" ]
+        if remainder:
+            parts.append(number_to_words(remainder))
+        return " and ".join(parts)
+
+    if number < 1000000:
+        factor = number / 1000
+        remainder = number % 1000
+        parts = [ number_to_words(factor) +" thousand" ]
+        if remainder:
+            parts.append(number_to_words(remainder))
+        if remainder < 100:
+            return " and ".join(parts)
+        return ", ".join(parts)            
+
+    factor = number / 1000000
+    remainder = number % 1000000
+    parts = [ number_to_words(factor) +" million" ]
+    if remainder:
+        parts.append(number_to_words(remainder))
+    if remainder < 100:
+        return " and ".join(parts)
+    return ", ".join(parts)            
+
+
+def pluralise(noun):
+    plural = {
+        "bison": "bison",
+        "goose": "geese",  # Irregular nouns
+        "moose": "moose",
+        "mouse": "mice",
+        "ox":    "oxen",
+        "sheep": "sheep",
+        "foot":  "feet",
+        "tooth": "teeth",
+        "man":   "men",
+        "woman": "women",
+        "child": "children",
+    }.get(noun, None)
+    if plural is not None:
+        return plural
+
+    sEnding = noun[-2:]
+    pEnding = {
+        "ss": "sses",   # bus      -> busses
+        "zz": "zzes",   # ?
+        "sh": "shes",   # bush     -> bushes
+        "ch": "ches",   # branch   -> branches
+        "fe": "ves",    # knife    -> knives
+        "ff": "ffs",    # cliff    -> cliffs
+
+        "ay": "ays",    # <vowel>y -> <vowel>ys
+        "ey": "eys",    #
+        "iy": "iys",    #
+        "oy": "oys",    #
+        "uy": "uys",    #
+    }.get(sEnding, None)
+    if pEnding is not None:
+        return noun[:-2] + pEnding
+
+    sEnding = noun[-1]
+    pEnding = {
+        "y": "ies",     # family   -> families
+        "f": "ves",     # loaf     -> loaves
+    }.get(sEnding, None)
+    if pEnding is not None:
+        return noun[:-1] + pEnding
+
+    pEnding = {
+        "s": "",        # pants    -> pants
+        "x": "es",      # fox      -> foxes
+    }.get(sEnding, None)
+    if pEnding is not None:
+        return noun + pEnding
+
+    # Fallback case.
+    logger.error("Failed to pluralise '%s'", noun)
+    return noun +"s"
+
+pluralize = pluralise   # American <- English
