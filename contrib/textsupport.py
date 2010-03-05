7838c4218c8625ee01295721a88c709cad84abc4
Richard Tew <richard.m.tew@gmail.com>
2010-03-05 06:02:37 +0000

    - contrib/textsupport.py: Added unit tests for pluralisation and number to words.  Also fixed bugs in number to words.
    git-svn-id: http://sorrows-mudlib.googlecode.com/svn/trunk@141 a9b543af-0a4c-0410-9886-61d43edf9ebb

--- contrib/textsupport.py
+++ contrib/textsupport.py
@@ -2,9 +2,7 @@
 This module is supposed to do useful things in the area of text formatting.
 """
 
-import StringIO, logging
-
-logger = logging.getLogger("textsupport")
+import StringIO
 
 
 def hcolumns(strings, width=80, marginSize=1, leftMarginSize=2, columnSize=None):
@@ -27,12 +25,9 @@ def hcolumns(strings, width=80, marginSize=1, leftMarginSize=2, columnSize=None)
 
 
 def number_to_words(number):
-    if number == 0:
-        return "none"
-
     if number < 20:
         return [
-            "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "none", "one", "two", "three", "four", "five", "six", "seven", "eight",
             "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
             "sixteen", "seventeen", "eighteen", "nineteen"
         ][number]
@@ -40,8 +35,9 @@ def number_to_words(number):
     if number < 100:
         factor = (number / 10) - 2
         remainder = number % 10
+        remainderString = ""
         if remainder:
-            remainderString = " "+ number_to_string(remainder)
+            remainderString = " "+ number_to_words(remainder)
         return [
             "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"
         ][factor] + remainderString
@@ -93,7 +89,7 @@ def pluralise(noun):
 
     sEnding = noun[-2:]
     pEnding = {
-        "ss": "sses",   # bus      -> busses
+        "ss": "sses",   # moss     -> mosses
         "zz": "zzes",   # ?
         "sh": "shes",   # bush     -> bushes
         "ch": "ches",   # branch   -> branches
@@ -125,7 +121,62 @@ def pluralise(noun):
         return noun + pEnding
 
     # Fallback case.
-    logger.error("Failed to pluralise '%s'", noun)
     return noun +"s"
 
 pluralize = pluralise   # American <- English
+
+if __name__ == "__main__":    
+    import unittest
+
+    class NumberWordificationTests(unittest.TestCase):
+        def testLessThanOneHundred(self):
+            self.failUnlessEqual(number_to_words(0), "none")
+            self.failUnlessEqual(number_to_words(1), "one")
+            self.failUnlessEqual(number_to_words(5), "five")
+            self.failUnlessEqual(number_to_words(10), "ten")
+            self.failUnlessEqual(number_to_words(15), "fifteen")
+            self.failUnlessEqual(number_to_words(20), "twenty")
+            self.failUnlessEqual(number_to_words(21), "twenty one")
+            self.failUnlessEqual(number_to_words(50), "fifty")
+
+        def testHundreds(self):
+            self.failUnlessEqual(number_to_words(100), "one hundred")
+            self.failUnlessEqual(number_to_words(101), "one hundred and one")
+            self.failUnlessEqual(number_to_words(111), "one hundred and eleven")
+            self.failUnlessEqual(number_to_words(199), "one hundred and ninety nine")
+            self.failUnlessEqual(number_to_words(999), "nine hundred and ninety nine")
+
+        def testThousands(self):
+            self.failUnlessEqual(number_to_words(1000), "one thousand")
+            self.failUnlessEqual(number_to_words(1001), "one thousand and one")
+            self.failUnlessEqual(number_to_words(1099), "one thousand and ninety nine")
+            self.failUnlessEqual(number_to_words(1100), "one thousand, one hundred")
+            self.failUnlessEqual(number_to_words(1101), "one thousand, one hundred and one")
+            self.failUnlessEqual(number_to_words(11101), "eleven thousand, one hundred and one")
+            self.failUnlessEqual(number_to_words(100101), "one hundred thousand, one hundred and one")
+            self.failUnlessEqual(number_to_words(101101), "one hundred and one thousand, one hundred and one")
+            
+        def testMillions(self):
+            self.failUnlessEqual(number_to_words(1000000), "one million")
+            self.failUnlessEqual(number_to_words(1000001), "one million and one")
+            self.failUnlessEqual(number_to_words(1900000), "one million, nine hundred thousand")
+            self.failUnlessEqual(number_to_words(1900001), "one million, nine hundred thousand and one")
+
+    class PluralisationTests(unittest.TestCase):
+        def testSelectionOfCases(self):
+            # One of the irregular nouns.    
+            self.failUnlessEqual(pluralise("man"), "men")
+            # Something other cases.
+            self.failUnlessEqual(pluralise("moss"), "mosses")
+            self.failUnlessEqual(pluralise("cliff"), "cliffs")    
+            self.failUnlessEqual(pluralise("knife"), "knives")
+            self.failUnlessEqual(pluralise("boy"), "boys")
+            self.failUnlessEqual(pluralise("grey"), "greys")
+            self.failUnlessEqual(pluralise("gray"), "grays")
+            self.failUnlessEqual(pluralise("nappy"), "nappies")
+            self.failUnlessEqual(pluralise("pants"), "pants")
+            self.failUnlessEqual(pluralise("fox"), "foxes")
+            # The fallback case.
+            self.failUnlessEqual(pluralise("chest"), "chests")
+
+    unittest.main()
