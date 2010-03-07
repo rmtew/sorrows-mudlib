0b43e4f4ad97f49509000a8b6c5f61cf2335a7bd
Richard Tew <richard.m.tew@gmail.com>
2010-03-07 08:38:33 +0000

    - shells/roguelike.py: Better input handling, where the server can handle ANSI escape codes (multi-character) as well as ANSI control codes.  Cursor keys now move the player around the view area, escape still quits the game, and control-r refreshes the screen completely.  Cleaned up the display code, it is ready to render the composite view (map, objects, npcs, player) whenever that is implemented.  Added functions to handle sending additional ANSI escape codes.
    git-svn-id: http://sorrows-mudlib.googlecode.com/svn/branches/7drl-2010@148 a9b543af-0a4c-0410-9886-61d43edf9ebb

--- games/7drl/shells/roguelike.py
+++ games/7drl/shells/roguelike.py
@@ -33,59 +33,66 @@ class RoguelikeShell(Shell):
         self.lastStatusBar = "-"
 
         self.user.connection.telneg.password_mode_on()
+        self.ShowCursor(False)
         self.DisplayScreen()
+
+    # ------------------------------------------------------------------------
+
+    def OnRemovalFromStack(self):
+        msg = ""
+
+        self.user.connection.optionLineMode = self.oldOptionLineMode
+        self.user.connection.telneg.request_will_echo()
+        self.user.Tell(EC_RESET_TERMINAL + EC_CLEAR_SCREEN + msg)
 
     def OnTerminalSizeChanged(self, rows, columns):
         self.DisplayScreen()
 
     def ReceiveInput(self, s):
-        for c in s:
-            self._ReceiveInput(c)
+        self._ReceiveInput(s)
 
-    def _ReceiveInput(self, c):
-        if c == chr(27):
+    def _ReceiveInput(self, s):
+        if s == chr(27): # Escape
             self.stack.Pop()
             self.user.ManualDisconnection()
             return
 
+        if s == chr(18):
+            self.lastStatusBar = " Screen refreshed"
+            self.DisplayScreen()
+            return
+        elif s == "\x1b[A": # Up cursor key.
+            if self.playerY > self.titleOffset + 1:
+                self.oldPlayerX = self.playerX
+                self.oldPlayerY = self.playerY
+                self.playerY -= 1
+                self.UpdateView()
+        elif s == "\x1b[B": # Down cursor key.
+            if self.playerY < self.statusOffset - 1:
+                self.oldPlayerX = self.playerX
+                self.oldPlayerY = self.playerY
+                self.playerY += 1
+                self.UpdateView()
+        elif s == "\x1b[C": # Right cursor key.
+            if self.playerX < self.user.connection.consoleColumns:        
+                self.oldPlayerX = self.playerX
+                self.oldPlayerY = self.playerY
+                self.playerX += 1
+                self.UpdateView()
+        elif s == "\x1b[D": # Left cursor key.
+            if self.playerX > 1:        
+                self.oldPlayerX = self.playerX
+                self.oldPlayerY = self.playerY
+                self.playerX -= 1
+                self.UpdateView()
+
         # Fallthrough.
-        s = "= %d" % ord(c)
-        self.UpdateStatusBar(" Unknown command \"%s\" %s" % (c, s))
+        t = str([ ord(c) for c in s ])
+        s = s.replace('\x1b', 'ESC')
+        s = "".join(c for c in s if ord(c) >= 32)
+        self.UpdateStatusBar(" Input: \"%s\" %s" % (s, t))
 
-    # ------------------------------------------------------------------------
-
-    def UpdateTitle(self):
-        pre = EC_HOME_CURSOR
-        
-        left = "  "+ sorrows.data.config.identity.name
-        right = self.status +"  "
-
-        self.user.Write(pre)
-        self.user.Write(EC_REVERSE_VIDEO_ON + left + (" " * (self.user.connection.consoleColumns - len(left) - len(right))) + right + EC_REVERSE_VIDEO_OFF)
-
-    def UpdateStatusBar(self, s):
-        self.lastStatusBar = s
-
-        self.MoveCursor(0, self.statusOffset, clear=True)
-        self.user.Write(EC_REVERSE_VIDEO_ON + s + (" " * (self.user.connection.consoleColumns - len(s))) + EC_REVERSE_VIDEO_OFF)
-        self.MoveCursor(0, self.statusOffset, clear=False)
-
-    def MoveCursor(self, x, y, clear=False):
-        s = "\x1b[%d;%dH" % (y, x)
-        if clear:
-            s += EC_CLEAR_LINE
-        self.user.Write(s)
-
-    def SetScrollingRows(self, yLow, yHigh):
-        self.user.Write("\x1b[%d;%dr" % (yLow, yHigh))
-
-    def ScrollWindowUp(self):
-        self.MoveCursor(self.windowOffset + 2)
-        self.user.Write(EC_SCROLL_UP)
-
-    def ScrollWindowDown(self):
-        self.MoveCursor(self.windowOffset)
-        self.user.Write(EC_SCROLL_DOWN)
+    # Display ----------------------------------------------------------------
 
     def DisplayScreen(self):
         self.user.Write(EC_CLEAR_SCREEN)
@@ -104,18 +111,86 @@ class RoguelikeShell(Shell):
 
         self.SetScrollingRows(self.windowOffset, self.windowEndOffset)
         # ----
-        windowLength = self.windowEndOffset - self.windowOffset + 1
-        for i in range(0, windowLength):
-            self.MoveCursor(0, self.windowOffset + i)
-            self.user.Write("%d" % i)
-        self.MoveCursor(0, self.statusOffset)
+
+        self.UpdateView()
 
         self.UpdateTitle()
         self.UpdateStatusBar(self.lastStatusBar)
 
-    def OnRemovalFromStack(self):
-        msg = "Simulation exited."
+    def UpdateView(self):
+        playerX = getattr(self, "playerX", None)
+        if playerX is None:
+            windowLength = self.windowEndOffset - self.windowOffset + 1
+            playerX = self.playerX = self.user.connection.consoleColumns / 2
+            playerY = self.playerY = self.windowOffset + windowLength/2
+            oldPlayerX = self.oldPlayerX = playerX
+            oldPlayerY = self.oldPlayerY = playerY
+        else:
+            playerY = self.playerY
+            oldPlayerX = self.oldPlayerX
+            oldPlayerY = self.oldPlayerY
 
-        self.user.connection.optionLineMode = self.oldOptionLineMode
-        self.user.connection.telneg.request_will_echo()
-        self.user.Tell(EC_RESET_TERMINAL + EC_CLEAR_SCREEN + msg)
+        if oldPlayerX != playerX or oldPlayerY != playerY:
+            self.MoveCursor(oldPlayerX, oldPlayerY)
+            self.EraseCharacters(1)
+
+        self.MoveCursor(playerX, playerY)
+        self.user.Write("@")
+
+    def UpdateTitle(self):
+        pre = EC_HOME_CURSOR
+        
+        left = "  "+ sorrows.data.config.identity.name
+        right = self.status +"  "
+
+        self.user.Write(pre)
+        self.user.Write(EC_REVERSE_VIDEO_ON + left + (" " * (self.user.connection.consoleColumns - len(left) - len(right))) + right + EC_REVERSE_VIDEO_OFF)
+
+    def UpdateStatusBar(self, s):
+        self.lastStatusBar = s
+
+        self.MoveCursor(0, self.statusOffset, clear=True)
+        self.user.Write(EC_REVERSE_VIDEO_ON + s + (" " * (self.user.connection.consoleColumns - len(s))) + EC_REVERSE_VIDEO_OFF)
+        self.MoveCursor(0, self.statusOffset, clear=False)
+
+    # ANSI Cursor ------------------------------------------------------------
+
+    def ShowCursor(self, flag=True):
+        if flag:
+            s = "\x1b[?25h"
+        else:
+            s = "\x1b[?25l"
+        self.user.Write(s)
+
+    def MoveCursor(self, x, y, clear=False):
+        s = "\x1b[%d;%dH" % (y, x)
+        if clear:
+            s += EC_CLEAR_LINE
+        self.user.Write(s)
+
+    def SaveCursorPosition(self):
+        self.user.Write("\x1b[s")
+        
+    def RestoreCursorPosition(self):
+        self.user.Write("\x1b[u")
+
+    # ANSI Scrolling ---------------------------------------------------------
+
+    def SetScrollingRows(self, yLow, yHigh):
+        self.user.Write("\x1b[%d;%dr" % (yLow, yHigh))
+
+    def ScrollWindowUp(self):
+        self.MoveCursor(self.windowOffset + 2)
+        self.user.Write(EC_SCROLL_UP)
+
+    def ScrollWindowDown(self):
+        self.MoveCursor(self.windowOffset)
+        self.user.Write(EC_SCROLL_DOWN)
+
+    # ANSI Attribute ---------------------------------------------------------
+
+    def RepeatLastCharacter(self, times=1):
+        self.user.Write("\x1b[%db" % times)
+
+    def EraseCharacters(self, times=1):
+        self.user.Write("\x1b[%dX" % times)
